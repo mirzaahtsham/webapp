4a322d11df54656e58e937d93906f9e704226b77
Mirza Ahtsham <mirza.ahtsham786@outlook.com>
2024-04-18 19:46:22 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+// import logo from './logo.svg';
+import './App.css';
+import Home from './components/Home';
+import Header from './components/header';
+import NavBar from './components/nav';
+function App() {
+  return (
+   <>
+   {/* <Header /> */}
+   <NavBar />
+   <Home />
+   </> 
+  );
+}
+
+export default App;
